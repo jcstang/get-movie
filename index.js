12c225228fb62f46eb22e81c40ed38370d019743
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-02 21:03:11 -0700

    added print method

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function getMovie(searchTerm, apiKey) {
   axios.get(queryURL)
     .then(function (response) {
       console.log(`movie response data:\n ${response.data}`);
+      printMovieResults(response.data);
       
       return response.data;
     })
@@ -20,8 +21,26 @@ function getMovie(searchTerm, apiKey) {
     });
 }
 
-function printMovieResults() {
+function printMovieResults(data) {
   //version 2
+  let movieData = data;
+
+      if (movieData.Response === 'False') {
+        return console.log(`No go!... ${movieData.Error}`);
+      } else {
+        // found a movie
+        console.log("-----------------------------------------------");
+        console.log(`Title:\t\t\t ${movieData.Title}`);
+        console.log(`Year:\t\t\t ${movieData.Year}`);
+        console.log(`IMDB rating:\t\t ${movieData.Ratings[0].Value}`);
+        console.log(`Rotten Tomatoes rating:\t ${movieData.Ratings[1].Value}`);
+        console.log(`Movie Produced in:\t ${movieData.Country}`);
+        console.log(`Language:\t\t ${movieData.Language}`);
+        console.log(`Plot:\t ${movieData.Plot}`);
+        console.log(`Actors:\t ${movieData.Actors}`);
+        console.log("-----------------------------------------------");
+
+      }
 }
 
 
